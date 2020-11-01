8744d8a3caf3fe97e17903839e7018029d896c37
Atai Isaev <Atai.Isaev.kx7@fh-zwickau.de>
2020-11-01 16:33:32 +0100

    add Cart class

---
+++ src/main/java/whz/pti/eva/praktikum_03/domain/Cart.java
@@ -0,0 +1,29 @@
+package whz.pti.eva.praktikum_03.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Cart {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private int quantity;
+    private String userId;
+
+    @OneToMany
+    private List<Item> items = new ArrayList<>();
+}
