9fae5dfd2fec8d2af493e4274147d2789c81de94
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-11-01 15:16:29 +0100

    init commit

---
+++ src/main/java/whz/pti/eva/praktikum_03/PizzaNurmanbetovIsaevApplication.java
@@ -0,0 +1,13 @@
+package whz.pti.eva.praktikum_03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaNurmanbetovIsaevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaNurmanbetovIsaevApplication.class, args);
+	}
+
+}
