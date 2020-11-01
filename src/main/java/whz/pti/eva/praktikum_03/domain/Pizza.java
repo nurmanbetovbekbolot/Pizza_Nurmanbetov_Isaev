fec5cc2323173750c53162064491c14dde00083e
Atai Isaev <Atai.Isaev.kx7@fh-zwickau.de>
2020-11-01 15:51:58 +0100

    add Pizza class

---
+++ src/main/java/whz/pti/eva/praktikum_03/domain/Pizza.java
@@ -0,0 +1,28 @@
+package whz.pti.eva.praktikum_03.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Pizza {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String name;
+    private BigDecimal priceLarge;
+    private BigDecimal priceMedium;
+    private BigDecimal priceSmall;
+
+
+}
