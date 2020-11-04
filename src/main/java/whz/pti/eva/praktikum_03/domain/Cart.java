7298a741bac3f98c51c80a761e7aa3adacb8c523
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-11-04 16:27:22 +0100

    add hashCode() and equals() in Ordered and OrderedItem class

--- src/main/java/whz/pti/eva/praktikum_03/domain/Cart.java
+++ src/main/java/whz/pti/eva/praktikum_03/domain/Cart.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -25,5 +24,5 @@ public class Cart {
     private String userId;
 
     @OneToMany
-    private List<Item> items = new ArrayList<>();
+    private List<Item> items;
 }
--- src/main/java/whz/pti/eva/praktikum_03/domain/OrderedItem.java
+++ src/main/java/whz/pti/eva/praktikum_03/domain/OrderedItem.java
@@ -24,4 +24,21 @@ public class OrderedItem {
 
     @Enumerated(EnumType.STRING)
     private PizzaSize size;
+
+    @Override
+    public int hashCode() {
+        if (getId() != null) {
+            return getId().hashCode();
+        }
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        OrderedItem other = (OrderedItem) obj;
+        return getId() != null && getId().equals(other.getId());
+    }
 }
